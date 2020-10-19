b0f1df3ba02024b13fe8dfc58e2369de74300ebc
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-10-18 21:58:38 -0400

    save

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     .then(res => res.json())
     .then(data => setUserArr(data));
   }
-  
 
   useEffect(() => { fetchUsers()}, [])
   const userLoggedIn = (username, password) => {
